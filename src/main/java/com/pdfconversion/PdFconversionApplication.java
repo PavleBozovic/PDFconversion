06c0c1a66ea72e25e3fe75eef9bd2ee7f1c99ccd
PavleBozovic <pavlebo34@gmail.com>
2025-12-08 10:05:01 +0100

    Creation of the project

---
+++ src/main/java/com/pdfconversion/PdFconversionApplication.java
@@ -0,0 +1,13 @@
+package com.pdfconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdFconversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdFconversionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pdfconversion/PdFconversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdfconversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdFconversionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
